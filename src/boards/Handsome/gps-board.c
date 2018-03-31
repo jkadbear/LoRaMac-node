d59dc92fe0c6beae6e8d13169d2c552beb3493fb
jkadbear <jkadbear@gmail.com>
2018-03-31 22:50:32 +0800

    Set nbChannels to 8 and add delay measurement

---
+++ src/boards/Handsome/gps-board.c
@@ -0,0 +1,49 @@
+/*!
+ * \file      gps-board.c
+ *
+ * \brief     Target board GPS driver implementation
+ *
+ * \copyright Revised BSD License, see section \ref LICENSE.
+ *
+ * \code
+ *               _______ _____ _____ _   _  _____ _    _ _    _
+ *              |__   __/ ____|_   _| \ | |/ ____| |  | | |  | |  /\
+ *                 | | | (___   | | |  \| | |  __| |__| | |  | | /  \
+ *                 | |  \___ \  | | | . ` | | |_ |  __  | |  | |/ /\ \
+ *                 | |  ____) |_| |_| |\  | |__| | |  | | |__| / ____ \
+ *                 |_| |_____/|_____|_| \_|\_____|_|  |_|\____/_/    \_\
+ *              (C)2017-2018 Tsinghua
+ *
+ * \endcode
+ *
+ * \author    jkadbear( Tsinghua )
+ */
+#include "board-config.h"
+#include "gpio.h"
+#include "gps.h"
+#include "rtc-board.h"
+#include "gps-board.h"
+
+void GpsMcuOnPpsSignal( void )
+{
+}
+
+void GpsMcuInvertPpsTrigger( void )
+{
+}
+
+void GpsMcuInit( void )
+{
+}
+
+void GpsMcuStart( void )
+{
+}
+
+void GpsMcuStop( void )
+{
+}
+
+void GpsMcuProcess( void )
+{
+}
